43551f22a46d2c3e291041798ccf3c7b6735be75
Romain Le Polh <romainlepolh@gmail.com>
2017-03-03 16:58:14 +0930

    Refacto module

--- bin/drupal-bower.js
+++ bin/drupal-bower.js
@@ -5,23 +5,36 @@ var argv = require('minimist')(process.argv.slice(2));
 var DrupalBower = require('../');
 
 var invoke = function(env) {
+  // Check if there is a .bowerrc file
+  // @todo Check all bowerrc file available
+  // @see https://bower.io/docs/config/
+  if (env.configFiles['.bower'].cwd === null) {
+    console.log('Unable to locate .bowerrc file.')
+    process.exit(0);
+  }
+  // Check the commands argv
   if (argv._.length == 0) {
     console.log('Use install of uninstall commads');
     process.exit(0);
   }
 
-  if (argv._[0] === 'install') {
-    DrupalBower.install(argv._.slice(1));
-  }
-
-  if (argv._[0] === 'uninstall') {
-    DrupalBower.uninstall(argv._.slice(1));
+  if (argv._[0] === 'install' || argv._[0] === 'uninstall') {
+    DrupalBower.exec(argv._[0], env, argv._.slice(1));
   }
 }
 
 const Cli = new Liftoff({
   name: 'DrupalBower',
+  configFiles: {
+    '.bower': {
+      cwd: '.'
+    }
+  },
+  extensions: {
+    'rc': null
+  },
   v8flags: ['--harmony']
 });
 
-Cli.launch({}, invoke);
+Cli.launch({
+}, invoke);
--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-var yaml = require('js-yaml');
-var fs = require('fs');
+'use strict';
 
-module.exports.install = function(assets) {
-    console.log(process.cwd());
-    console.log('Install ' + assets)
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+var BowerDrupal = function() {
+  this.vendorDir = null,
+  this.debug = true
+}
+/**
+ * Entry function
+ */
+BowerDrupal.prototype.exec = function (action, env, assets) {
+  this.setVendorDir(env.configFiles['.bower'].cwd)
+  this.loadLibraries()
+}
+/**
+ * Get the vendor directory form .bowerrc file
+ */
+BowerDrupal.prototype.setVendorDir = function (bowerFilePath) {
+  var doc = yaml.safeLoad(fs.readFileSync(bowerFilePath), 'utf-8')
+  if (doc.hasOwnProperty('directory')) {
+    this.vendorDir = doc.directory
+  } else {
+    this.vendorDir = 'bower_components/'
+  }
+  if (this.debug) {
+    console.log('Set vendorDir: ' + this.vendorDir)
+  }
+}
+/**
+ * Load current libraries from drupal.libraries.yml file
+ */
+BowerDrupal.prototype.loadLibraries = function () {
+  var filePattern = '*.libraries.yml'
+  var libraries = yaml.safeLoad(fs.readFileSyc)
 }
 
-module.exports.uninstall = function(assets) {
-    console.log('Uninstall ' + assets)
-}
+module.exports = new BowerDrupal();
